b925c7521187128157365da1a2d654989aa795ee
dannyvassallo <danielvassallo87@gmail.com>
2016-10-07 15:23:51 -0400

    form now validates input

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,2 +1,54 @@
-// ADD YOUR JS HERE
-console.log('loaded');
+// iterate through errors and growl them
+function toasts(){
+  setTimeout(function(){
+    $('span.error').each(function(){
+      if($(this).html() != ""){
+        var errorText = $(this).text();
+        Materialize.toast(errorText, 4000, 'red break-word');
+      }
+    });
+  }, 100);
+}
+
+
+$("#shirtform").validate({
+  ignore: "",
+  focusInvalid: false,
+  rules: {
+    'entry.920194180': {
+      required: true
+    },
+    'entry.1622627988': {
+      required: true
+    },
+    'entry.380080146': {
+      required: true
+    }
+  },
+  messages: {
+    'entry.920194180': {
+      required: "Please give your full name."
+    },
+    'entry.1622627988': {
+      required: "Please give your shipping address."
+    },
+    'entry.380080146': {
+      required: "Please select a shirt size."
+    }
+  },
+  invalidHandler: function(form, validator) {
+    toasts();
+  },
+  success: "valid",
+  submitHandler: function(form) {
+    form.submit();
+    Materialize.toast("Thanks you! Your order was sent!", 4000, 'green');
+    setTimeout(function(){
+      form.empty();
+    }, 1000);
+  },
+  errorElement : 'span',
+  errorPlacement: function (error, element) {
+    error.insertAfter($(element).next());
+  }
+});
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,5 @@
 //= require jquery
 //= require materialize-sprockets
 //= require _init-materialize
+//= require _jquery.validate.min
 //= require _custom
